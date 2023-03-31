6cff70d1aa53273245b3e9f3dd6f1d39ce10aa6d
Abcacode4r <128299791+Abcacode4r@users.noreply.github.com>
2023-03-31 07:16:35 -0900

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,21 +1,30 @@
 from turtle import Turtle
 ALIGNMENT="center"
-FONT=("Arial",18,"normal")
+FONT=("Courier",18,"bold")
 class Score_board(Turtle):
     def __init__(self):
         super().__init__()
         self.score=0
+        self.high_score=0
+        with open("data.txt") as data:
+            self.high_score=int(data.read())
         self.color("white")
         self.pu()
         self.hideturtle()
         self.goto(0,270)
-        self.write(f"score:{self.score}",align=ALIGNMENT,font=FONT)
+        self.write(f"score:{self.score} High_score:{self.high_score}",align=ALIGNMENT,font=FONT)
     def update_score(self):
         self.clear()
         self.score+=1
-        self.write(f"score:{self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"score:{self.score} High_score:{self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write(f"GAME OVER!", align="center", font=("Arial", 24, "normal"))
+    def reset(self):
+        if self.score>self.high_score:
+            self.high_score=self.score
+            with open("data.txt",mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.clear()
+        self.score=0
+        self.write(f"score:{self.score} High_score:{self.high_score}", align=ALIGNMENT, font=FONT)
+
 
